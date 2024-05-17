2d13a91fa8e9c55bdb8c051f8b620f5c658aab37
Matthew Wishek <matthew@wishek.com>
2024-05-16 20:19:55 -0600

    Remove low-pass filter from loop-filter

--- src/loop_filter.vhd
+++ src/loop_filter.vhd
@@ -16,7 +16,6 @@ ENTITY loop_filter IS
 		clk 			: IN  std_logic;
 		init 			: IN  std_logic;
 
-		lpf_alpha 		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
 		lpf_p_gain 		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
 		lpf_i_gain 		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
 		lpf_freeze 	 	: IN  std_logic;
@@ -37,7 +36,6 @@ ARCHITECTURE rtl OF loop_filter IS
 	SIGNAL p_val : signed(ERR_W -1 DOWNTO 0);
 
 	SIGNAL lpf_err_valid_d  : std_logic;
-	SIGNAL lpf_err_valid_dd : std_logic;
 
 	SIGNAL acc : signed(NCO_W -1 DOWNTO 0);
 	SIGNAL sum : signed(NCO_W -1 DOWNTO 0);
@@ -48,16 +46,22 @@ BEGIN
 	BEGIN
 		IF clk'EVENT AND clk = '1' THEN
 
-			lpf_err_valid_dd <= lpf_err_valid_d;
+			lpf_err_valid_d <= lpf_err_valid;
 
-			IF lpf_err_valid_d = '1' AND lpf_freeze = '0' THEN
-				i_acc <= i_acc + acc;
-				i_val <= resize(i_acc * signed(lpf_i_gain), NCO_W);
+			IF lpf_err_valid = '1' AND lpf_freeze = '0' THEN
+				i_acc <= i_acc + resize(signed(lpf_err), NCO_W);
+				i_val <= resize(shift_right(i_acc * signed(lpf_i_gain), 8), NCO_W);
 			END IF;
 
 			IF lpf_zero = '1' OR init = '1' THEN
 				i_acc <= (OTHERS => '0');
 				i_val <= (OTHERS => '0');
+			END IF;
+
+			IF init = '1' THEN
+				lpf_err_valid_d <= '0';
+				i_acc			<= (OTHERS => '0');
+				i_val 			<= (OTHERS => '0');
 			END IF;
 
 		END IF;
@@ -67,8 +71,12 @@ BEGIN
 	BEGIN
 		IF clk'EVENT AND clk = '1' THEN
 
-			IF lpf_err_valid_d = '1' THEN
-				p_val <= resize(signed(lpf_p_gain) * signed(lpf_err), ERR_W);
+			IF lpf_err_valid = '1' THEN
+				p_val <= resize(shift_right(signed(lpf_p_gain) * signed(lpf_err), 4), ERR_W);
+			END IF;
+
+			IF init = '1' THEN
+				p_val <= (OTHERS => '0');
 			END IF;
 
 		END IF;
@@ -78,7 +86,7 @@ BEGIN
 	BEGIN
 		IF clk'EVENT AND clk = '1' THEN
 
-			IF lpf_err_valid_dd = '1' THEN
+			IF lpf_err_valid_d = '1' THEN
 				IF INVERT_FADJ THEN
 					lpf_adjust <= std_logic_vector(NOT (p_val + i_val) + 1);
 				ELSE
@@ -89,27 +97,12 @@ BEGIN
 				lpf_adj_valid <= '0';
 			END IF;
 
+			IF init = '1' THEN
+				lpf_adjust 		<= (OTHERS => '0');
+				lpf_adj_valid	<= '0';
+			END IF;
+
 		END IF;
 	END PROCESS sum_proc;
-
-	sum <= shift_right(signed(lpf_err) - acc, to_integer(signed(lpf_alpha)));
-
-	iir_proc : PROCESS (clk)
-	BEGIN
-		IF clk'EVENT AND clk = '1' THEN
-
-			lpf_err_valid_d <= lpf_err_valid;
-
-			IF lpf_err_valid = '1' THEN
-				acc <= acc + sum;
-			END IF;
-
-			IF init = '1' THEN
-				acc <= (OTHERS => '0');
-			END IF;
-
-		END IF;
-	END PROCESS iir_proc;
-
 
 END ARCHITECTURE rtl;
